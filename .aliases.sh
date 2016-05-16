182e8a51fde04021fc38bdb8b1bfbd35dd9b6c41
Sven212 <sven212@everfall.de>
2016-05-16 17:54:19 +0200

    Update dotfiles

---
+++ .aliases.sh
@@ -0,0 +1,30 @@
+#Git
+alias gi="git init"
+alias gs="git status"
+alias gd="git diff"
+alias gdh="git diff HEAD"
+alias gc="git clone $1 $2"
+alias gcm="git commit -m "$1""
+alias gaa="git add -A ."
+alias gpo="git push origin $1"
+alias gl="git log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
+alias glo="git log --oneline"
+alias grhh="git reset --hard HEAD"
+alias gcp="git cherry-pick $1"
+
+# Arch
+alias ya="yaourt  --nameonly --pager --color --noconfirm $1"
+
+# Files
+alias l="ls -o -hX --group-directories-first"
+alias la="ls -o -AhX --group-directories-first"
+alias v='nvim'
+alias vim='nvim'
+
+# Config
+alias termiteconf="nvim ~/.config/termite/config"
+alias zshrc="nvim ~/.zshrc && source ~/.zshrc"
+
+alias update.="gaa && gcm 'Update dotfiles' && gpo master"
+
+alias h='tldr'
---
+++ .zshrc
@@ -0,0 +1,14 @@
+source /usr/share/zsh/scripts/antigen/antigen.zsh
+
+antigen use oh-my-zsh
+antigen bundle jump
+
+antigen theme honukai
+
+antigen apply
+
+antigen bundle zsh-users/zsh-syntax-highlighting
+antigen bundle zsh-users/zsh-autosuggestions
+
+source ~/.aliases.sh
+
