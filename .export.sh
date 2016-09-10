34d86d2d2c08fa4f3a2019693e71bfc27d4796c6
sven212 <sven212@everfall.de>
2016-09-10 17:27:46 +0200

    removed GRAILS_HOME from PATH

--- .export.sh
+++ .export.sh
@@ -1,5 +1,4 @@
 export EDITOR=nvim
 export JAVA_HOME='/usr/lib/jvm/java-8-jdk'
-export GRAILS_HOME='/home/sven212/bin/grails-2.5.1/'
 
-export PATH=$GRAILS_HOME/bin:$JAVA_HOME/bin:$PATH
+export PATH=$JAVA_HOME/bin:$PATH
