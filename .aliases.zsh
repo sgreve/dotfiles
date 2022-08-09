05acdb1fc5324ab31c67802281f591ffb3c6a68a
Sven Greve <git@sgreve.me>
2022-08-09 20:01:49 +0200

    inital commit

---
+++ .aliases.zsh
@@ -0,0 +1,17 @@
+# Git
+alias gi="git init"
+alias gs="git status"
+alias gd="git diff"
+alias gdh="git diff HEAD"
+alias gc="git clone --recursive $1 $2"
+alias gcm="git commit -m "$1""
+alias gaa="git add -A ."
+alias gpo="git push origin $1"
+alias gl="git log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
+alias glo="git log --oneline"
+alias grhh="git reset --hard HEAD"
+alias gcp="git cherry-pick $1"
+alias update.="gaa && gcm 'Update dotfiles' && gpo master"
+
+# Generic
+alias ls="exa -lBhmbU"
---
+++ .zshrc
@@ -0,0 +1,16 @@
+# Pre-Check
+if [[ -e $HOME/dotfiles ]]; then
+	DOTFILES=$HOME/dotfiles
+else
+	echo "dotfiles dir not found, please use git clone --recursive inside your home dir" >&2
+fi 
+
+# Antidote 
+[[ -e $DOTFILES/.antidote ]] || git clone https://github.com/mattmc3/antidote.git $DOTFILES/.antidote
+source "$DOTFILES/.antidote/antidote.zsh"
+antidote load $DOTFILES/.zsh_plugins.txt
+setopt prompt_subst
+
+# Load the rest
+source "$DOTFILES/.aliases.zsh"
+source "$DOTFILES/.export.zsh"
