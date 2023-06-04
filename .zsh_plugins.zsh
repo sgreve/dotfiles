2c268338fd91043e90439344ef57f657d350f1b7
Sven Greve <git@sgreve.me>
2023-06-04 02:52:25 +0200

    Updated FIle structure

--- .zsh_plugins.zsh
+++ .zsh_plugins.zsh
@@ -1,9 +1,7 @@
-fpath+=( /Users/svengreve/Library/Caches/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-completions )
-source /Users/svengreve/Library/Caches/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-completions/zsh-completions.plugin.zsh
-fpath+=( /Users/svengreve/Library/Caches/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-syntax-highlighting )
-source /Users/svengreve/Library/Caches/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-syntax-highlighting/zsh-syntax-highlighting.plugin.zsh
-fpath+=( /Users/svengreve/Library/Caches/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-autosuggestions )
-source /Users/svengreve/Library/Caches/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-autosuggestions/zsh-autosuggestions.plugin.zsh
-fpath+=( /Users/svengreve/Library/Caches/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-ohmyzsh-SLASH-ohmyzsh )
-source /Users/svengreve/Library/Caches/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-ohmyzsh-SLASH-ohmyzsh/oh-my-zsh.sh
-source /Users/svengreve/Library/Caches/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-oskarkrawczyk-SLASH-honukai-iterm-zsh/./honukai.zsh-theme
+zsh-users/zsh-completions
+zsh-users/zsh-syntax-highlighting
+zsh-users/zsh-autosuggestions
+
+ohmyzsh/ohmyzsh
+
+oskarkrawczyk/honukai-iterm-zsh path:./honukai.zsh-theme
--- .zshrc
+++ .zshrc
@@ -8,7 +8,7 @@ fi
 # Antidote 
 [[ -e $DOTFILES/.antidote ]] || git clone https://github.com/mattmc3/antidote.git $DOTFILES/.antidote
 source "$DOTFILES/.antidote/antidote.zsh"
-antidote load $DOTFILES/.zsh_plugins.txt
+antidote load $DOTFILES/.zsh_plugins.zsh
 setopt prompt_subst
 
 # Load the rest
